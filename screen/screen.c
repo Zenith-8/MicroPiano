4baee8c063d09579ca80469510acf12d424ddf1d
plyantix <152106812+plyantix@users.noreply.github.com>
2026-03-08 14:32:10 -0400

    working example for screen

--- screen/screen.c
+++ screen/screen.c
@@ -1,36 +1,23 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/spi.h"
 
-// SPI Defines
-// We are going to use SPI 0, and allocate it to the following GPIO pins
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
+#include "ili9341.h"
+#include "gfx.h"
 
-
+int c;
 
 int main()
 {
-    stdio_init_all();
+    LCD_setPins(20, 17, 21, 18, 19);
 
-    // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    LCD_initDisplay();
+    LCD_setRotation(1);
+    GFX_createFramebuf();
+    while (true)
+    {
+        GFX_clearScreen();
+        GFX_setCursor(0, 0);
+        GFX_printf("Hello GFX!\n%d", c++);
+        GFX_flush();
+        sleep_ms(500);
     }
 }
